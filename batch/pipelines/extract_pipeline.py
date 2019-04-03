e2fbdedb44d9caaeda3a9abe8706b9e16632e035
Alexey Taymanov <ataymano@microsoft.com>
2019-04-03 14:45:44 -0400

    sweep (1 node)

--- batch/pipelines/extract_pipeline.py
+++ batch/pipelines/extract_pipeline.py
@@ -1,27 +1,29 @@
 import application
-from steps import create_cache_step
+from steps import create_cache_step, vw_sweep_step, vw_predict_step
 from azureml.pipeline.core import Pipeline
-from steps import vw_predict_step
 from azureml.pipeline.core.graph import PipelineParameter
 
 
-def create_pipeline(ws, ctx, parallel_jobs):
+def create_pipeline(ws, ctx, process_per_node):
     cacheStep = create_cache_step.create_cache_step(
         workspace=ws,
         context=ctx
     )
 
-    predict = vw_predict_step.vw_predict_step(
-        workspace=ws,
-        input_folder=cacheStep.output,
-        commandline=PipelineParameter(
-            name='extra_2',
-            default_value='--cb_explore_adf --epsilon 0.2 --dsjson'
-        ),
-        policy_name='NoMarginal'
-    )
+    sweepStep = vw_sweep_step.vw_sweep_step(workspace=ws,
+                                            input_folder = cacheStep.output,
+                                            process_per_node = PipelineParameter(name = "procs", default_value = '1'))
+   # predict = vw_predict_step.vw_predict_step(
+   #     workspace=ws,
+   #     input_folder=cacheStep.output,
+   #     commandline=PipelineParameter(
+   #         name='extra_2',
+   #         default_value='--cb_explore_adf --epsilon 0.2 --dsjson'
+   #     ),
+   #     policy_name='NoMarginal'
+   # )
 
-    extractPipeline = Pipeline(workspace=ws, steps=[cacheStep, predict])
+    extractPipeline = Pipeline(workspace=ws, steps=[sweepStep.step])
     print ("extractPipeline is succesfully created.")
 
     extractPipeline.validate()
