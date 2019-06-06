f9af2fef6f2469766ee8051db0534db829517a16
Alexey Taymanov <ataymano@gmail.com>
2019-06-05 21:49:41 -0400

    more complex scenario

--- icml_demo/demo.py
+++ icml_demo/demo.py
@@ -4,70 +4,82 @@ from msrest.authentication import CognitiveServicesCredentials
 from helpers import SlidingAverage
 
 from datetime import datetime
+from random import randint
 
-client = PersonalizerClient(endpoint="https://westus2.api.cognitive.microsoft.com/",
-    credentials=CognitiveServicesCredentials("793eed0bedff47beb87919daed625ff4"))   # Put your credentials here
+def run(user_preference, actions, client, duration_sec):
+    start = datetime.now()
+    ctr = SlidingAverage(window_size = 20)
+    while (datetime.now() - start).total_seconds() < duration_sec:
+        index = randint(0, len(user_preference) - 1)
+        
+        user = user_preference[index][0]
+        preference = user_preference[index][1]
 
-user=[{'age': 20}]
-actions=[
-    models.RankableAction(
-        id='politics',
-        features=[{'topic': 'politics'}]),
-    models.RankableAction(
-        id='sports',
-        features=[{'topic': 'sports'}])]
+        request=models.RankRequest(
+            context_features=user,
+            actions=actions
+        )
+        response=client.rank(request)
+        reward = 1.0 if response.reward_action_id==preference else 0.0
+        client.events.reward(event_id=response.event_id, value=reward)
 
-request=models.RankRequest(
-    context_features=user,
-    actions=actions
-)
+        ctr.update(reward)
+        print('CTR: ' + str(ctr.get()))
+        for action in response.ranking:
+            print(action.id + ': ' + str(action.probability))
 
-# Since we are doing cold start and there is no model, all probabilities are the same
-response=client.rank(request)
-for action in response.ranking:
-    print(action.id + ': ' + str(action.probability))
 
-duration_s = 200
-ctr = SlidingAverage(window_size=50)
+def main():
+    client = PersonalizerClient(endpoint="https://westus2.api.cognitive.microsoft.com/",
+        credentials=CognitiveServicesCredentials(""))   # Put your credentials here
 
-print("User loves politics")
-start = datetime.now()
-while (datetime.now() - start).total_seconds() < duration_s:
+    #Available content
+    actions=[
+        models.RankableAction(
+            id='politics',
+            features=[{'topic': 'politics'}]),
+        models.RankableAction(
+            id='sports',
+            features=[{'topic': 'sports'}]),
+        models.RankableAction(
+            id='music',
+            features=[{'topic': 'music'}]
+        )]
+
+    #User features
+    Tom = {'name': 'Tom'}
+    Anna = {'name': 'Anna'}
+
+    #Time features
+    Monday = {'day': 'Monday'}
+    Sunday = {'day': 'Sunday'}
+
+
+    request=models.RankRequest(
+        context_features=[Tom, Monday],
+        actions=actions
+    )
+
+    # Since we are doing cold start and there is no model, all probabilities are the same
     response=client.rank(request)
-    reward = 1.0 if response.reward_action_id=="politics" else 0.0
-    client.events.reward(event_id=response.event_id, value=reward)
-
-    ctr.update(reward)
-    print('CTR: ' + str(ctr.get()))
     for action in response.ranking:
         print(action.id + ': ' + str(action.probability))
 
-print("------------------------------------------")
-print("User changes his preferences to sports")
-print("------------------------------------------")
 
-start = datetime.now()
-while (datetime.now() - start).total_seconds() < 2 * duration_s:
-    response=client.rank(request)
-    reward = 1.0 if response.reward_action_id=="sports" else 0.0
-    client.events.reward(event_id=response.event_id, value=reward)
+    # Tom and Anna have certain preferences what to read on Monday and Sunday
+    scenario = [([Tom, Monday], 'politics'),
+                ([Tom, Sunday], 'music'),
+                ([Anna, Monday], 'sports'),
+                ([Anna, Sunday], 'politics')]
 
-    ctr.update(reward)
-    print('CTR: ' + str(ctr.get()))
-    for action in response.ranking:
-        print(action.id + ': ' + str(action.probability))
+    run(scenario, actions, client, 600)
 
-#Please run counterfactual evaluation and import result policy to your loop
-#Then uncomment the following block and execute it
+    # Olympics started and both Tom and Anna are following sport news during weekend
+    scenario = [([Tom, Monday], 'politics'),
+                ([Tom, Sunday], 'sports'),
+                ([Anna, Monday], 'sports'),
+                ([Anna, Sunday], 'sports')]
+    run(scenario, actions, client, 600)
 
-#print("User changes his preferences back to politics")
-#ctr = SlidingAverage(window_size=10)
-#for i in range(1, 5 * n):
-#    response=client.rank(request)
-#    reward = 1.0 if response.reward_action_id=="politics" else 0.0
-#    client.events.reward(event_id=response.event_id, value=reward)
-    
-#    ctr.update(reward)
-#    print('CTR: ' + str(ctr.get()))
-#    for action in response.ranking:
-#        print(action.id + ': ' + str(action.probability))
+if __name__== '__main__':
+  main()
