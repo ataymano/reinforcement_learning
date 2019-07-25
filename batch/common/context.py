f3b2f2463736d34bdeac4bb628f488c0acb00122
Alexey Taymanov <ataymano@microsoft.com>
2019-07-24 22:14:50 -0400

    sparse tensors support

---
+++ batch/common/context.py
@@ -0,0 +1,6 @@
+class Context:
+    def __init__(self, account_name, account_key, container, folder):
+        self.account_name = account_name
+        self.account_key = account_key
+        self.container = container
+        self.folder = folder
