68a79a68bcbbdd1ff7a8cf73be916407bfb1e385
Alexey Taymanov <ataymano@gmail.com>
2020-03-02 11:06:34 -0500

    model deployment

--- batch/common/context.py
+++ batch/common/context.py
@@ -4,3 +4,14 @@ class Context:
         self.account_key = account_key
         self.container = container
         self.folder = folder
+    
+    def GetBlockBlobService(self):
+        from azure.storage.blob import BlockBlobService
+        return BlockBlobService(
+            account_name=self.account_name,
+            account_key=self.account_key
+        )
+
+    def deploy_model(self, model_path):
+        service = self.GetBlockBlobService()
+        service.create_blob_from_path(container_name=self.container, blob_name='current', file_path=model_path)
